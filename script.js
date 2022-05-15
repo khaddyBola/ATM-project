220e324614a3461bb91d146b8d79df7b6aa87819
khaddyBola <khadijahmusa232@gmail.com>
2022-05-15 17:40:22 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,30 +1,79 @@
 'use strict'
-const ATMCard = document.querySelector('.ATMCard')
-const overlay = document.querySelector('.overlay');
-const btnForm = document.querySelector('.btnForm');
+const firstName = document.getElementById('FirstName')
+const lastName = document.getElementById('LastName');
+const accountType = document.getElementById('account-type');
+const submitBtn = document.getElementById('submit')
+
+
+const accountTypeText = document.querySelector('.account-type');
+const cardDigit = document.querySelector('.card-digit');
+const expDate = document.querySelector('.expiry-date');
+const firstNameText = document.querySelector('.firstname');
+const lastNameText = document.querySelector('.lastname');
+
+const atmExc = document.querySelector('.atm-Exc');
+const atmBox = document.querySelector('.atmBox')
 
 
 
-function btnF() {
-    ATMCard.classList.add('hidden');
-    overlay.classList.add('hidden');
+//Function
+const genCardDigit = () => {
+    const randomNum = Math.trunc(Math.random() * 1000000000000000) + '';
+    const lastFour = randomNum.slice(-4);
+    const getNum = lastFour.padStart(randomNum.length, '*')
+    return getNum;
+
 }
 
-function btnFN() {
-    ATMCard.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+
+const genFirstName = (nameStr) => {
+    const [firstXter, ...otherXter] = nameStr;
+    return [firstXter.toUpperCase(), ...otherXter].join('');
+}
+
+const genLastName = (nameStr) => {
+    return nameStr.toUpperCase();
 }
 
 
-btnForm.addEventListener('click', () => {
-    // console.log('is clicked')
-    if (ATMCard.style.display === 'none') {
-        ATMCard.style.display = 'block';
-        ATMCard.innerHTML = 'hide list'
-    } else {
-        ATMCard.style.display = 'none'
-    }
+const genExpDate = () => {
+    const d = new Date();
+    const month =d.getMonth() +1;
+    const fixMonth = month < 10 ? '0' + month : month;
+    const year = (d.getFullYear() + 3)+'';
+    return `${fixMonth}/${year.slice(-2)}`
+
+}
 
 
- 
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    // console.log('submit button')
+    // Display ATM
+    atmExc.classList.add('hidden');
+    atmBox.classList.remove('hidden')
+
+    // Display Account type
+    accountTypeText.textContent = accountType.value;
+
+    // Display the card digit
+    cardDigit.textContent = genCardDigit();
+
+    //Display EXpire Date
+    expDate.textContent = genExpDate();
+
+    //Display first and lastname
+    firstNameText.textContent = genFirstName(firstName.value)
+    lastNameText.textContent = genLastName(lastName.value)
 })
+
+
+
+
+
+
+
+
+
+
